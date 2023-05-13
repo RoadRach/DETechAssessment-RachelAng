f348c7b69a55c311929e8af394dbc9f178d490bd
RoadRach <70367556+RoadRach@users.noreply.github.com>
2023-05-13 20:04:03 +0800

    Prepare for refactoring

--- Section-1-Data_Pipelines/dags/new_membership.py
+++ Section-1-Data_Pipelines/dags/new_membership.py
@@ -18,27 +18,31 @@ default_args = {
 
 def membership_etl():
     @task()
-    def read_csv():
+    def read_csv(ti):
         import pandas as pd
 
         url = 'https://raw.githubusercontent.com/RoadRach/DETechAssessment-RachelAng/main/Section-1-Data_Pipelines/dags/data/applications_dataset_1.csv'
         url2 = 'https://raw.githubusercontent.com/RoadRach/DETechAssessment-RachelAng/main/Section-1-Data_Pipelines/dags/data/applications_dataset_2.csv'
+        url3 = 'https://raw.githubusercontent.com/RoadRach/DETechAssessment-RachelAng/main/Section-1-Data_Pipelines/dags/applications_dataset_test1.csv'
         
-        df = pd.concat(map(pd.read_csv, [url, url2]), ignore_index=True)
+        df = pd.concat([pd.read_csv(url2), pd.read_csv(url3)], ignore_index=True)
 
         print(df)
-        return df
+        ti.xcom_push(df=df)
+        # return df
         
     @task()
-    def rm_no_name(df):
+    def rm_no_name(ti):
         import pandas as pd
+        df=ti.xcom_pull(task_ids='read_csv', key='read_csv')
+        sdf = df.drop(df[df.name == ''].index)
+        udf = df[df.name == ''].index
+        print(udf)
+        # return df
+        print("Sigh")
 
-        print("Hello!")
-        df = df.drop(df[df.name == ''].index)
-
-        print(df)
 
     read_csv=read_csv()
-    rm_no_name = rm_no_name(df=read_csv)
+    rm_no_name = rm_no_name()
 
 membership_dag = membership_etl()
