a3474db15985e103c4056838c27369efe2824e36
RoadRach <70367556+RoadRach@users.noreply.github.com>
2023-05-13 20:59:26 +0800

    Push working DAG

--- Section-1-Data_Pipelines/dags/new_membership.py
+++ Section-1-Data_Pipelines/dags/new_membership.py
@@ -18,31 +18,32 @@ default_args = {
 
 def membership_etl():
     @task()
-    def read_csv(ti):
+    def read_csv():
         import pandas as pd
 
         url = 'https://raw.githubusercontent.com/RoadRach/DETechAssessment-RachelAng/main/Section-1-Data_Pipelines/dags/data/applications_dataset_1.csv'
         url2 = 'https://raw.githubusercontent.com/RoadRach/DETechAssessment-RachelAng/main/Section-1-Data_Pipelines/dags/data/applications_dataset_2.csv'
         url3 = 'https://raw.githubusercontent.com/RoadRach/DETechAssessment-RachelAng/main/Section-1-Data_Pipelines/dags/applications_dataset_test1.csv'
         
-        df = pd.concat([pd.read_csv(url2), pd.read_csv(url3)], ignore_index=True)
+        df = pd.concat(map(pd.read_csv, [url, url3]), ignore_index=True)
 
         print(df)
-        ti.xcom_push(df=df)
-        # return df
+        # ti.xcom_push(df=df)
+        jdf = pd.DataFrame.to_json(df)
+        return jdf
         
     @task()
-    def rm_no_name(ti):
+    def rm_no_name(jdf):
         import pandas as pd
-        df=ti.xcom_pull(task_ids='read_csv', key='read_csv')
-        sdf = df.drop(df[df.name == ''].index)
-        udf = df[df.name == ''].index
-        print(udf)
+        # df=ti.xcom_pull(task_ids='read_csv', key='read_csv')
+        # sdf = df.drop(df[df.name == ''].index)
+        # udf = df[df.name == ''].index
+        # print(udf)
         # return df
         print("Sigh")
 
 
     read_csv=read_csv()
-    rm_no_name = rm_no_name()
+    rm_no_name = rm_no_name(jdf=read_csv)
 
 membership_dag = membership_etl()
