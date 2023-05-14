8ce9b2ea5aebf9b57e5f174f5848fccf09bc8f3c
RoadRach <70367556+RoadRach@users.noreply.github.com>
2023-05-14 08:56:41 +0800

    dropped no names from main df

--- Section-1-Data_Pipelines/cron_job/cron_application.py
+++ Section-1-Data_Pipelines/cron_job/cron_application.py
@@ -9,14 +9,16 @@ import pandas as pd
 csv = ['applications_dataset_1.csv', 'applications_dataset_2.csv']
 df = pd.concat(map(pd.read_csv, csv), ignore_index=True)
 
-print(df)
-
-def p_name(name):
-    return HumanName(name)
-
-df['parsed_name'] = df['name'].apply(p_name)
-
-df['First Name'] = df['parsed_name'].apply(lambda x: x.first)
-df['Last Name'] = df['parsed_name'].apply(lambda x: x.last)
+df = df.dropna(subset='name').reset_index(drop=True)
 
 print(df)
+
+# def p_name(name):
+#     return HumanName(name)
+
+# df['parsed_name'] = df['name'].apply(p_name)
+
+# df['First Name'] = df['parsed_name'].apply(lambda x: x.first)
+# df['Last Name'] = df['parsed_name'].apply(lambda x: x.last)
+
+# print(df)
